1137397669fbf1670475b5d9878b2dae154141d4
moulika-pixel <84400427+moulika-pixel@users.noreply.github.com>
2021-07-05 04:48:22 -0700

    Add files via upload

---
+++ numberplate.py
@@ -0,0 +1,125 @@
+import cv2
+import imutils
+import numpy as np
+import pytesseract
+import datetime
+import ibm_boto3
+from ibm_botocore.client import Config, ClientError
+import sys
+import ibmiotf.application
+import ibmiotf.device
+import random
+import time
+
+from cloudant.client import Cloudant
+from cloudant.error import CloudantException 
+from cloudant.result import Result, ResultByKey
+from PIL import Image
+
+
+
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+img = cv2.imread('1.jpg',cv2.IMREAD_COLOR)
+img = imutils.resize(img, width=500 )
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert to grey scale
+gray = cv2.bilateralFilter(gray, 11, 17, 17) #Blur to reduce noise
+edged = cv2.Canny(gray, 30, 200) #Perform Edge detection
+cnts,new = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+img1=img.copy()
+cv2.drawContours(img1,cnts,-1,(0,255,0),3)
+cv2.imshow("img1",img1)
+cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:30]
+screenCnt = None #will store the number plate contour
+img2 = img.copy()
+cv2.drawContours(img2,cnts,-1,(0,255,0),3) 
+cv2.imshow("img2",img2) #top 30 contours
+
+count=0
+idx=7
+# loop over contours
+for c in cnts:
+  # approximate the contour
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.018 * peri, True)
+        if len(approx) == 4: #chooses contours with 4 corners
+                screenCnt = approx
+                x,y,w,h = cv2.boundingRect(c) #finds co-ordinates of the plate
+                new_img=img[y:y+h,x:x+w]
+                cv2.imwrite('./'+str(idx)+'.png',new_img) #stores the new image
+                idx+=1
+                break
+            #draws the selected contour on original image        
+cv2.drawContours(img, [screenCnt], -1, (0, 255, 0), 3)
+cv2.imshow("Final image with plate detected",img)
+
+Cropped_loc='./7.png' #the filename of cropped image
+cv2.imshow("cropped",cv2.imread(Cropped_loc))
+pytesseract.pytesseract.tesseract_cmd=r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe" #exe file for using ocr 
+
+text=pytesseract.image_to_string(Cropped_loc,lang='eng') #converts image characters to string
+print("Number is:" ,text)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+COS_ENDPOINT = "https://s3.jp-tok.cloud-object-storage.appdomain.cloud"
+#s3.us-south.cloud-object-storage.appdomain.cloud" # Current list avaiable at https://control.cloud-object-storage.cloud.ibm.com/v2/endpoints
+COS_API_KEY_ID = "vpT8qb32mbGKPkWHMMyV_Ockvrm4M-RMmawGmUwx191U" # eg "W00YiRnLW4a3fTjMB-oiB-2ySfTrFBIQQWanc--P3byk"
+COS_AUTH_ENDPOINT = "https://iam.cloud.ibm.com/identity/token"
+COS_RESOURCE_CRN = "crn:v1:bluemix:public:cloud-object-storage:global:a/1d9c20f9423f45b09aefda7babaa80c7:a29ca935-4475-4841-9f49-fc95d40c3741::"
+
+client = Cloudant("apikey-v2-1g3gbh54jnpvsvp5gjhxyjntrus3j4v9mixdm2ikf29l", "ba8a1a3d4de1f91f6aaafb81df51788e", url="https://apikey-v2-1g3gbh54jnpvsvp5gjhxyjntrus3j4v9mixdm2ikf29l:ba8a1a3d4de1f91f6aaafb81df51788e@4fa55c95-7b54-4ad0-b044-93f45808d078-bluemix.cloudantnosqldb.appdomain.cloud")
+client.connect()
+database_name = "voshal14"  
+
+# Create resource
+cos = ibm_boto3.resource("s3",
+    ibm_api_key_id=COS_API_KEY_ID,
+    ibm_service_instance_id=COS_RESOURCE_CRN,
+    ibm_auth_endpoint=COS_AUTH_ENDPOINT,
+    config=Config(signature_version="oauth"),
+    endpoint_url=COS_ENDPOINT
+)
+    
+        
+def multi_part_upload(bucket_name, item_name, file_path):
+    try:
+        print("Starting file transfer for {0} to bucket: {1}\n".format(item_name, bucket_name))
+        # set 5 MB chunks
+        part_size = 1024 * 1024 * 5
+
+        # set threadhold to 15 MB
+        file_threshold = 1024 * 1024 * 15
+
+        # set the transfer threshold and chunk size
+        transfer_config = ibm_boto3.s3.transfer.TransferConfig(
+            multipart_threshold=file_threshold,
+            multipart_chunksize=part_size
+        )
+
+        # the upload_fileobj method will automatically execute a multi-part upload
+        # in 5 MB chunks for all files over 15 MB
+        with open(file_path, "rb") as file_data:
+            cos.Object(bucket_name, item_name).upload_fileobj(
+                Fileobj=file_data,
+                Config=transfer_config
+            )
+
+        print("Transfer for {0} Complete!\n".format(item_name))
+    except ClientError as be:
+        print("CLIENT ERROR: {0}\n".format(be))
+    except Exception as e:
+        print("Unable to complete multi-part upload: {0}".format(e))
+
+    my_database = client.create_database(database_name)        
+    multi_part_upload("voshal5","car.jpg","C:/Users/vosha/Desktop/IOT/SmartInternz-IoT-Externship-2021-master/Smart Home security codes/1.jpg")      
+    if my_database.exists():
+        print("'{database_name}' successfully created.")
+        json_document = {
+                "_id": pic,
+                "link":"https://voshal5.s3.jp-tok.cloud-object-storage.appdomain.cloud/"+picname
+                }
+
+            
+multi_part_upload("voshal5","car.jpg","C:/Users/vosha/Desktop/IOT/SmartInternz-IoT-Externship-2021-master/Smart Home security codes/1.jpg")
